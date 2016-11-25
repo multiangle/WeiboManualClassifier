a817b5ce84dc3a207e737262f3b1184a9c8c597a
multiangle <461445957@qq.com>
2016-11-25 18:26:54 +0800

    main page

--- main.js
+++ main.js
@@ -9,8 +9,11 @@ const Menu = electron.Menu ;        // menu item
 
 let main_win ;
 
-app.on('ready',)
+app.on('ready',createWindow)
 
+app.on('window-all-closed',()=>{
+    if (process.platform !== 'darwin') app.quit() ;
+})
 
 function createWindow(){
     main_win = new BrowserWindow({
@@ -19,5 +22,23 @@ function createWindow(){
         backgroundColor: '#FFEEBF',
         show:false
     })
+    
+    main_win.loadURL(url.format({
+        pathname: path.join(__dirname,'sections/main.html'),
+        protocol: 'file:', // pay attention ':'
+        slashes: true
+    }))
 
+    // oepn dev tools
+    main_win.webContents.openDevTools()
+
+    main_win.on('closed',()=>{
+        main_win = null ;
+    })
+
+    main_win.once('ready-to-show',()=>{
+        main_win.show() ;
+    })
 }
+
+
